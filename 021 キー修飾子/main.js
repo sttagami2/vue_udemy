b7ff986f209d5e1ce96249ac3d666852c1757b57
sttagami2 <sttagami2@gmail.com>
2020-06-14 21:54:34 +0900

    キー修飾子

---
+++ 021 キー修飾子/main.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: '#app',
+  data: {
+    number: 0,
+    x: 0,
+    y: 0,
+  },
+  methods: {
+    countUp: function(times) {
+      this.number += 1 * times
+    },
+    changeMousePosition: function(divideNumber, event) {
+      this.x = event.clientX / divideNumber;
+      this.y = event.clientY / divideNumber;
+    },
+    myAlert() {
+      alert('アラート');
+    }
+  }
+})

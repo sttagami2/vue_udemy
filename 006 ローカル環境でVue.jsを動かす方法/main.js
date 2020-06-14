d88502f9c31013569c901343d2fea76b08026e44
sttagami2 <sttagami2@gmail.com>
2020-06-14 14:58:49 +0900

    ローカル環境でVue.jsを動かす方法

---
+++ 006 ローカル環境でVue.jsを動かす方法/main.js
@@ -0,0 +1,12 @@
+new Vue({
+  el: '#app',                                         // htmlファイル内のどこを対象にするかを定義  el=element=要素
+  data: {
+    message: 'Hello World!'
+  },
+  methods: {                                          // 処理方法を定義できる
+    reverseMessage: function() {                      // htmlファイル内のv-onが定義されているボタンが押されたらreverseMessageを実行する
+                                                      // this.messageは#appが定義されているタブ内の{{message}}のことをさす
+      this.message = this.message.split('').reverse().join('');
+    }
+  }
+})

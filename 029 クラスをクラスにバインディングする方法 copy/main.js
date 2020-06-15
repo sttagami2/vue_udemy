29288e3e9e6a2a8ac16d4f26a4053ad28d64e2f4
sttagami2 <sttagami2@gmail.com>
2020-06-15 12:56:09 +0900

    クラスをデータにバインディングする方法

---
+++ 029 クラスをクラスにバインディングする方法 copy/main.js
@@ -0,0 +1,14 @@
+new Vue({
+  el: '#app',
+  data: {
+    isActive: true
+  },
+  computed: {
+    classObject: function() {
+      return{
+        red: this.isActive,
+        'bg-ground': !this.Active
+      }
+    }
+  }
+})
